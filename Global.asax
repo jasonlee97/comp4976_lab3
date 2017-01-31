6db2086b9380faa7e68cbe9f7fea0669c97be52d
Jason Lee <jasonlee1054@gmail.com>
2017-01-30 20:25:12 -0800

    first commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="comp4976_lab3.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(comp4976_lab3.Startup))]
+namespace comp4976_lab3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
